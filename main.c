39fafdc49a9f17922655c30e732518d69c7e7ca1
Alicja Kałuża <92887428+alicja832@users.noreply.github.com>
2023-10-24 15:03:36 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,116 +1,75 @@
-#include "Board_Buttons.h"              // ::Board Support:Buttons
 #include "Board_LED.h"                  // ::Board Support:LED
-#include "RTE_Device.h"                 // Keil::Device:Startup
-#include "Driver_USART.h"               // ::CMSIS Driver:USART
-#include "Board_Joystick.h"             // ::Board Support:Joystick
+#include "Board_Buttons.h"              // ::Board Support:Buttons
 #include "LPC17xx.h"                    // Device header
+#include "Board_Joystick.h"             // ::Board Support:Joystick
+#include "Driver_USART.h"               // ::CMSIS Driver:USART
 
-volatile uint32_t msTicks = 0;
+#include <stdio.h>
+#include <string.h>
 
+#define LED_NUMBER 8
 
-void conf(void)
-{
-	//dlatego 10 poniewaz chcemy aby umiescic taka wartosc aby zegar wykonywal przerwanie raz na 0,1 s
-	SysTick_Config(SystemCoreClock/10);
-}
-void SysTick_Handler(void)
-{
+volatile int msTicks = 0;
+
+void SysTick_Handler(void) {
 	msTicks++;
 }
 
-void delay(int d)
-{
-			msTicks=0;
-			while(d>msTicks);
+void delay(int t) {
+	msTicks = 0;
+	while(t>msTicks);
 }
 
-//jest zamontowany USART1 na plytce
 extern ARM_DRIVER_USART Driver_USART1;
 static ARM_DRIVER_USART * USARTdrv = &Driver_USART1;
-char cmd;
+   
+char                   cmd;
 
 
 
-int main(void)
-{
-	conf();
+
+int main(int argc, char** argv) {
 	
+	uint32_t retC;
+	retC = SysTick_Config(SystemCoreClock/10);
+	//int LEDState = 0;
 	Buttons_Initialize();
-	//wtedy sie wszystkie diody sie swieca
+	Joystick_Initialize ();
 	LED_Initialize();
-	//inicjalizacja joysticka
-	Joystick_Initialize();
+
+	USARTdrv->Initialize(NULL);    
+	USARTdrv->PowerControl(ARM_POWER_FULL);
+	USARTdrv->Control(ARM_USART_MODE_ASYNCHRONOUS |
+									ARM_USART_DATA_BITS_8 |                      
+								ARM_USART_PARITY_NONE |                      
+								ARM_USART_STOP_BITS_1 |                      
+									ARM_USART_FLOW_CONTROL_NONE, 4800);        
+USARTdrv->Control (ARM_USART_CONTROL_TX, 1);    
+USARTdrv->Control (ARM_USART_CONTROL_RX, 1);     
+USARTdrv->Send("\nPress Enter to receive a message", 34);
 	
-	//zmienna do zapalania kolejno lampek
-	int num=0;
 	
-	//przesylanie wiadomosci na terminal 
-	USARTdrv->Initialize(NULL);
-    /*Power up the USART peripheral */
-  USARTdrv->PowerControl(ARM_POWER_FULL);
-    /*Configure the USART to 4800 Bits/sec */
-  USARTdrv->Control(ARM_USART_MODE_ASYNCHRONOUS |
-                      ARM_USART_DATA_BITS_8 |
-                      ARM_USART_PARITY_NONE |
-                      ARM_USART_STOP_BITS_1 |
-                      ARM_USART_FLOW_CONTROL_NONE, 4800);
-     
-    /* Enable Receiver and Transmitter lines */
-   USARTdrv->Control (ARM_USART_CONTROL_TX, 1);
-   USARTdrv->Control (ARM_USART_CONTROL_RX, 1);
- 
-   USARTdrv->Send("\nPress Enter to receive a message", 34);
-    
-	while(1)
-	{
-		//druga lampka miga-zapala sie i wlacza przy kazdej petli
-		LED_On(2);
-		
-		//lampki gasza sie kolejno
-		if(Buttons_GetState())
-				LED_Off(num);
-		else{
-			LED_On(num);
-			num++;
-		}
-		switch(Joystick_GetState())
-		{
-			case(JOYSTICK_DOWN):
-			{
-				LED_Off(1);
-				delay(1);
-				LED_On(1);
-				break;
-				
-			}
-			case(JOYSTICK_UP):
-			{
-				LED_Off(2);
-				delay(1);
-				LED_On(2);
-				break;
-			}
-			case(JOYSTICK_RIGHT):
-			{
-				LED_Off(3);
-				delay(1);
-				LED_On(3);
-				break;
-			}
-			case(JOYSTICK_LEFT):
-			{
-				LED_Off(0);
-				delay(1);
-				LED_On(0);
-				break;
-			}
-		}
-		delay(1);
-		LED_Off (2);
-		delay(1);
-		
-		if(num>7)
-			num=0;
-		
+	
+	for(int i = 0; i<8; i++) {
+		LED_Off(i);
 	}
+	while(1) {
+		LED_On(0);
+		delay(5);
+		LED_Off(0);
+		delay(5);
+	}
+	
+	return 0;
 }
+
+/*
+if(LEDState) LED_On(0);
+		else LED_Off(0);
+		while(Buttons_GetState()&(1 << 0 )) {
+			delay(100);
+				if(Buttons_GetState()&(1 << 0 )) {
+					if(LEDState) LEDState=0;
+					else LEDState = 1;
+				}
+*/
