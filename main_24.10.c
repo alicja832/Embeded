7f50d14bfcd9a440c266c5ae019e74adbfe74b25
Alicja Kałuża <92887428+alicja832@users.noreply.github.com>
2023-10-29 14:47:38 +0100

    Update main_24.10.c

--- main_24.10.c
+++ main_24.10.c
@@ -2,13 +2,9 @@
 #include "PIN_LPC17xx.h"                // Keil::Device:PIN
 #include "Driver_USART.h"               // ::CMSIS Driver:USART
 
-/*extern ARM_DRIVER_USART Driver_USART0;
-static ARM_DRIVER_USART * USARTdrv = &Driver_USART0;
-char                   cmd;*/
 
 int main(void)
 {
-    //conf();
     PIN_Configure(0,2,1,0,0);
     PIN_Configure(0,3,1,0,0);
 
@@ -33,27 +29,6 @@ int main(void)
             LPC_UART0 -> THR = LPC_UART0 -> RBR;
     }
 
-    /*USARTdrv->Initialize(NULL);
-    USARTdrv->PowerControl(ARM_POWER_FULL);
-    USARTdrv->Control(ARM_USART_MODE_ASYNCHRONOUS |
-    ARM_USART_DATA_BITS_8 |
-    ARM_USART_PARITY_NONE |
-    ARM_USART_STOP_BITS_1 |
-    ARM_USART_FLOW_CONTROL_NONE, 4800);
-    USARTdrv->Control (ARM_USART_CONTROL_TX, 1);
-    USARTdrv->Control (ARM_USART_CONTROL_RX, 1);
-    
-
-
-    USARTdrv->Send("\nPress Enter to receive a message", 34);
-    while(1)
-
-    USARTdrv->Receive(&cmd, 1);
-		//kod ascii 13 //jest ok
-        if(cmd==13)
-        {
-            USARTdrv->Send("\nHello World!", 12);
-        }
-    }*/
+   
     return 0;
 }
