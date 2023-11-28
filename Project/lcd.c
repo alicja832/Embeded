6417fcd126030bafa273542cb7eace659326aa93
Alicja Kałuża <92887428+alicja832@users.noreply.github.com>
2023-11-28 20:05:43 +0100

    Create lcd.c

---
+++ Project/lcd.c
@@ -0,0 +1,8 @@
+
+		//tutaj jakbysmy chcieli okno narysowac wydajniej
+	lcdWriteReg(HADRPOS_RAM_START,  0);
+	lcdWriteReg(HADRPOS_RAM_END,  100);
+	lcdWriteReg(VADRPOS_RAM_START,  0);
+	lcdWriteReg(VADRPOS_RAM_END,  100);
+	lcdWriteReg(ENTRYM,3);
+	
