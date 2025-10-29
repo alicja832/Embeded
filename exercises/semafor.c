2f68809bc9a8bb1868a401e9d5580aae68195acf
Alicja Kałuża <92887428+alicja832@users.noreply.github.com>
2025-10-29 17:45:21 +0100

    Implement semaphore-based LED control in semafor.c

---
+++ exercises/semafor.c
@@ -0,0 +1,84 @@
+#include "cmsis_os2.h"
+#include "Board_LED.h"                  // ::Board Support:LE
+#include "RTE_Device.h"                 // Keil::Device:Startup
+#include "rtx_os.h"
+osSemaphoreId_t sid_Semaphore; 
+uint16_t off=0;
+void Timer1_Callback (void *arg)
+{
+	LED_On(3);
+	
+}	// prototypes for timer callback function
+
+void Timer2_Callback (void *arg)
+{
+	LED_Off(3);
+	
+}	// prototypes for timer callback function
+
+void threadacquire(void *tmp)
+{
+	while(1)
+	{
+		if(off) {
+			LED_On(3);
+			off = 0;
+		}
+		else
+		{
+			LED_Off(3);
+			off = 1;
+		}
+		osStatus_t val = osSemaphoreAcquire(sid_Semaphore,100000);  
+	}
+}
+void threadrelease(void *tmp)
+{
+	while(1)
+	{
+		osDelay(1000);
+		osSemaphoreRelease(sid_Semaphore);
+		
+	}		
+}
+void app_main(void * tmp)
+{
+	uint32_t exec1= 1U; 
+	sid_Semaphore = osSemaphoreNew(20U, 20U, NULL);
+
+    //osTimerId_t id1 = osTimerNew(Timer1_Callback, osTimerPeriodic, &exec1,NULL);
+	//osTimerId_t id2 = osTimerNew(Timer2_Callback, osTimerPeriodic, &exec1,NULL);
+
+	uint32_t timerDelay = 2000U;
+	
+	//osTimerStart(id1,timerDelay);
+	//osDelay(1000);
+	//osTimerStart(id2,timerDelay);
+	while(1)
+	{
+		
+		osDelay(1000);
+	}
+	
+}
+
+
+
+int main()
+{
+	
+	SystemCoreClockUpdate();
+	osKernelInitialize();
+	
+	//wtedy sie wszystkie diody sie swieca
+	LED_Initialize();
+    osThreadNew(app_main,NULL,NULL);
+	osThreadNew(threadacquire,NULL,NULL);
+	osThreadNew(threadrelease,NULL,NULL);
+
+	osKernelStart();
+	while(1)
+	{
+		
+	}
+}
